cc1913ac91887a8e787f6b9346bb71c0c05a899d
IvanLuengas <ing.ivanluengas@hotmail.com>
2021-06-09 15:06:50 -0500

    Principio

---
+++ resources/views/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Perros y Canes</title>
+	<link rel="stylesheet" type="text/css" href="../css/stile.css">
+</head>
+<body lang="es">
+	<header>
+		<img src="../image/perrosycanes.jpg">
+		<h1>Perros y Canes</h1>
+	</header>
+
+	<nav>
+		<ul>
+			<li>Inicio</li>
+			<li>Razas</li>
+			<li>Concejos</li>
+			<li>Blog</li>
+		</ul>
+	</nav>
+
+	<section>
+		<article>
+			<hgroup>
+				<h2>Springer Spaniel</h2>
+			</hgroup>
+				<p>El Springer es un perro mediano. La diferencia en apariencia entre el field y el bench springer spaniel inglés se debe a la especialización por parte de los criadores y de entrada se distinguirá por los colores, marcaciones y cantidad de pelo. El bench Springer tendrá un manto oscuro simétricamente repartido en todo el cuerpo con orejas largas y oscuras, collar blanco, patas blancas y muchos flecos. El Field Springer tendrá más blanco, casi a punto de ser completamente blanco, con cabeza y orejas, es muy gentil juguetón también tiene algunos parches de color en el cuerpo a veces con motitas.</p>
+				<p>En segundo plano, las diferencias se notarán en la conformación de la cabeza, largo de cuerpo y largo de rabo. El Field Springer tendrá un hocico más puntiagudo con menos labio y las orejas serán implantadas más altas. Será más largo de lomo y debido a que el rabo es el mayor indicador que el perro haya olfateado un pájaro, será cortada más larga.</p>
+				<p>El Field Springer es el máximo en atletismo y performance. El bench Springer es un soberbio ejemplar de tipicidad, sanidad y simetría. Pero ambos cumplen con su propósito original: el de acompañante del cazador y ambos combinan la belleza, la fuerza y la calidad al máximo.</p>
+				<p>Los Springer Spaniels del continente Europeo tienen muchas motitas sin cuidado al reparto de manchas, son menos angulosos, más altos, compactos y cuadrados. </p>
+				<footer>
+					<p>Comentarios</p>
+				</footer>
+				<address>
+					<a href="#">Leer mas</a>
+				</address>
+		</article>
+	</section>
+	<aside>
+		<blockquote cite="http://ivanluengas.com"></blockquote>
+		Mensaje 1
+	</aside>
+	<footer>
+		Derechos reservados 2010 - 2021
+	</footer>
+
+</body>
+</html>
